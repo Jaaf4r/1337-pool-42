b5032d5199acac4164924045dfa86ca937d3c6ba
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-09-18 18:18:13 +0100

    1337

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int	nbr;
+	
+	nbr = nb;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nbr *= -1;
+	}
+	if (nbr < 10)
+	{
+		ft_putchar(nbr + '0');
+	}
+	else
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+}
