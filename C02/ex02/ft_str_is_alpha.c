a95aa1613d3c0e334d78f2636dd4ab6a460f0e77
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-09-22 15:28:59 +0100

    1337

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,29 @@
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	w[]="123/*?.,";
+	char	x[]="abcdefjh";
+	char	y[]="abcd0fjh";
+	char	z[]="";
+
+	printf("it should print 0 | %d\n", ft_str_is_alpha(w));
+	printf("it should print 1 | %d\n", ft_str_is_alpha(x));
+	printf("it should print 0 | %d\n", ft_str_is_alpha(y));
+	printf("it should print 1 | %d\n", ft_str_is_alpha(z));
+}*/
