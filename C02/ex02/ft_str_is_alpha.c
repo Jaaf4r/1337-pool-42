f582dabdc30750ab4e78b1c369eb67caed0d328c
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-09-23 17:00:28 +0100

    1337

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -5,10 +5,9 @@ int	ft_str_is_alpha(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
-			i++;
-		else
+		if (!(str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
 			return (0);
+		i++;
 	}
 	return (1);
 }
