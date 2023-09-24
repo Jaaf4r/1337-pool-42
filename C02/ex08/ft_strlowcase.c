8f8ee6d86e9bdbdb7aacdce16b4c87de21dacb0b
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-09-24 20:29:48 +0100

    1337

--- C02/ex08/ft_strlowcase.c
+++ C02/ex08/ft_strlowcase.c
@@ -9,3 +9,16 @@ char	*ft_strlowcase(char *str)
 			str[i] += 32;
 		i++;
 	}
+	return (str);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	x[]="FREEDOM";
+	char	y[]="THiRTeEN tHIrTY SevEN";
+
+	printf("%s\n", ft_strlowcase(x));
+	printf("%s\n", ft_strlowcase(y));
+}
