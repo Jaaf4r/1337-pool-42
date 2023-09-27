691958ea25239d441cef1041771063575a30e006
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-09-27 18:29:14 +0100

    1337

---
+++ C02/ex09/ft_strcapitalize.c
@@ -0,0 +1,47 @@
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] +=32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	flag;
+
+	i = 0;
+	flag = 1;
+	ft_strlowcase(str);
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (flag == 1)
+				str[i] -= 32;
+			flag = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			flag = 0;
+		else
+			flag = 1;
+		i++;
+	}
+	return (str);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	x[]="salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+
+	printf("%s\n", ft_strcapitalize(x));
+}
