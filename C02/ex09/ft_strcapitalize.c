2053d084c4b6451e1abe8c9a82b23457e7bd3709
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-09-27 18:30:02 +0100

    1337

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -36,7 +36,7 @@ char	*ft_strcapitalize(char *str)
 	}
 	return (str);
 }
-
+/*
 #include <stdio.h>
 
 int	main(void)
@@ -44,4 +44,4 @@ int	main(void)
 	char	x[]="salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
 
 	printf("%s\n", ft_strcapitalize(x));
-}
+}*/
