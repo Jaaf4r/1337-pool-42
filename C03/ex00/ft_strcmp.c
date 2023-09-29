4ebc0712738cdb925b5c1d0079a07b9a15e97d8c
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-09-29 15:05:46 +0100

    1337

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -7,10 +7,10 @@ int	ft_strcmp(char *s1, char *s2)
 		i++;
 	return (s1[i] - s2[i]);
 }
-
+/*
 #include <stdio.h>
 
 int	main(void)
 {
 	printf("%d\n", ft_strcmp("hello world!", "hello worlz!"));
-}
+}*/
