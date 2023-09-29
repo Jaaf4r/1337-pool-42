828cb47dbeda3b7600d5a09ccb5809ca131c3f6b
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-09-29 15:35:02 +0100

    1337

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,28 @@
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	x[]="Hello ";
+	char	y[]="world!";
+
+	printf("%s\n", ft_strcat(x, y));
+}
