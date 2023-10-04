7b6ad304ea607884a7bf8ee9eb7fcacf057b91a0
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-10-04 18:43:41 +0100

    1337

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -1,12 +1,42 @@
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+int	ft_strlen(char *str)
 {
-	unsigned int i;
-	unsigned int j;
+	int	i;
 
 	i = 0;
-	j = 0;
-	if (src[j] == '\0')
-		return (dest);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	destlen;
+	unsigned int	srclen;
+
+	destlen = ft_strlen(dest);
+	srclen = ft_strlen(src);
+	i = 0;
 	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j] && j < size)
 	{
-		while (dest[i + j] && src[j] &&
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	x[10]="Hello, ";
+	char	y[10]="world!";
+
+	printf("The new string is %s | Its full length is \n", ft_strlcat(x, y, 2));
+}
