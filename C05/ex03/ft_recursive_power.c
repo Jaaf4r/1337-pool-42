f6ff84efb9649f9d09a57f8f27b3c4c7372f42ce
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-10-08 13:23:01 +0100

    1337

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -11,10 +11,10 @@ int	ft_recursive_power(int nb, int power)
 		result = nb * ft_recursive_power(nb, power - 1);
 	return (result);
 }
-/*
+
 #include <stdio.h>
 
 int	main(void)
 {
 	printf("%d\n", ft_recursive_power(2, 3));
-}*/
+}
