7de77d2dbd5fbab7ab798d0985f8325555ff8e16
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-11-29 21:49:51 +0100

    1337

---
+++ Exam01/3-inter/inter.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+
+int	check_doubles(char *str, char c, int pos)
+{
+	int	i;
+
+	i = 0;
+	while (i < pos)
+	{
+		if (str[i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	inter(char *s1, char *s2)
+{
+	int	i, j;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		j = 0;
+		while (s2[j] != '\0')
+		{
+			if (s1[i] == s2[j])
+			{
+				if (check_doubles(s1, s1[i], i) == 1)
+				{
+					write(1, &s1[i], 1);
+					break;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		inter(av[1], av[2]);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
